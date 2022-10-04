ab53bb2e4c886ec678954ce44579ad01e05ad1da
Vanessa Payr <vanessa.payr@hak-kitz.ac.at>
2022-10-04 12:49:44 +0200

    Test02

--- SWP-4IT-WP-VP/compare.Designer.cs
+++ SWP-4IT-WP-VP/compare.Designer.cs
@@ -28,84 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_compare = new System.Windows.Forms.Button();
-            this.lbl_chooseINV = new System.Windows.Forms.Label();
-            this.cb_chooseTable = new System.Windows.Forms.ComboBox();
-            this.lbl_Compare = new System.Windows.Forms.Label();
-            this.cb_compareto = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
-            // 
-            // btn_compare
-            // 
-            this.btn_compare.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_compare.Location = new System.Drawing.Point(19, 199);
-            this.btn_compare.Name = "btn_compare";
-            this.btn_compare.Size = new System.Drawing.Size(259, 51);
-            this.btn_compare.TabIndex = 1;
-            this.btn_compare.Text = "Compare";
-            this.btn_compare.UseVisualStyleBackColor = true;
-            this.btn_compare.Click += new System.EventHandler(this.btn_compare_Click);
-            // 
-            // lbl_chooseINV
-            // 
-            this.lbl_chooseINV.AutoSize = true;
-            this.lbl_chooseINV.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_chooseINV.Location = new System.Drawing.Point(68, 9);
-            this.lbl_chooseINV.Name = "lbl_chooseINV";
-            this.lbl_chooseINV.Size = new System.Drawing.Size(154, 24);
-            this.lbl_chooseINV.TabIndex = 2;
-            this.lbl_chooseINV.Text = "choose Inventory";
-            // 
-            // cb_chooseTable
-            // 
-            this.cb_chooseTable.FormattingEnabled = true;
-            this.cb_chooseTable.Location = new System.Drawing.Point(16, 36);
-            this.cb_chooseTable.Name = "cb_chooseTable";
-            this.cb_chooseTable.Size = new System.Drawing.Size(263, 21);
-            this.cb_chooseTable.TabIndex = 3;
-            this.cb_chooseTable.SelectedIndexChanged += new System.EventHandler(this.cb_chooseTable_SelectedIndexChanged);
-            // 
-            // lbl_Compare
-            // 
-            this.lbl_Compare.AutoSize = true;
-            this.lbl_Compare.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Compare.Location = new System.Drawing.Point(90, 124);
-            this.lbl_Compare.Name = "lbl_Compare";
-            this.lbl_Compare.Size = new System.Drawing.Size(121, 24);
-            this.lbl_Compare.TabIndex = 4;
-            this.lbl_Compare.Text = "Compare To:";
-            // 
-            // cb_compareto
-            // 
-            this.cb_compareto.FormattingEnabled = true;
-            this.cb_compareto.Location = new System.Drawing.Point(16, 151);
-            this.cb_compareto.Name = "cb_compareto";
-            this.cb_compareto.Size = new System.Drawing.Size(263, 21);
-            this.cb_compareto.TabIndex = 5;
             // 
             // compare
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(290, 275);
-            this.Controls.Add(this.cb_compareto);
-            this.Controls.Add(this.lbl_Compare);
-            this.Controls.Add(this.cb_chooseTable);
-            this.Controls.Add(this.lbl_chooseINV);
-            this.Controls.Add(this.btn_compare);
+            this.ClientSize = new System.Drawing.Size(387, 338);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "compare";
             this.Text = "Compare";
             this.Load += new System.EventHandler(this.Compare_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button btn_compare;
-        private System.Windows.Forms.Label lbl_chooseINV;
-        private System.Windows.Forms.ComboBox cb_chooseTable;
-        private System.Windows.Forms.Label lbl_Compare;
-        private System.Windows.Forms.ComboBox cb_compareto;
     }
 }
--- SWP-4IT-WP-VP/login.Designer.cs
+++ SWP-4IT-WP-VP/login.Designer.cs
@@ -28,59 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_Login = new System.Windows.Forms.Button();
-            this.txtbox_username = new System.Windows.Forms.TextBox();
-            this.txtbox_password = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // btn_Login
-            // 
-            this.btn_Login.Location = new System.Drawing.Point(324, 274);
-            this.btn_Login.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btn_Login.Name = "btn_Login";
-            this.btn_Login.Size = new System.Drawing.Size(164, 54);
-            this.btn_Login.TabIndex = 0;
-            this.btn_Login.Text = "Login";
-            this.btn_Login.UseVisualStyleBackColor = true;
-            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
-            // 
-            // txtbox_username
-            // 
-            this.txtbox_username.Location = new System.Drawing.Point(237, 111);
-            this.txtbox_username.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtbox_username.Name = "txtbox_username";
-            this.txtbox_username.Size = new System.Drawing.Size(317, 22);
-            this.txtbox_username.TabIndex = 1;
-            // 
-            // txtbox_password
-            // 
-            this.txtbox_password.Location = new System.Drawing.Point(237, 183);
-            this.txtbox_password.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtbox_password.Name = "txtbox_password";
-            this.txtbox_password.Size = new System.Drawing.Size(317, 22);
-            this.txtbox_password.TabIndex = 2;
             // 
             // login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtbox_password);
-            this.Controls.Add(this.txtbox_username);
-            this.Controls.Add(this.btn_Login);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "login";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btn_Login;
-        private System.Windows.Forms.TextBox txtbox_username;
-        private System.Windows.Forms.TextBox txtbox_password;
     }
 }
