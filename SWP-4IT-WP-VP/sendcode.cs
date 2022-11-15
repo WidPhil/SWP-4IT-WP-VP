db5aa5db9e974afdc99228f1708e11cf38f77e86
WidPhil <91456693+WidPhil@users.noreply.github.com>
2022-11-15 12:52:54 +0100

    Verfication Code

--- SWP-4IT-WP-VP/sendcode.cs
+++ SWP-4IT-WP-VP/sendcode.cs
@@ -14,9 +14,7 @@ namespace SWP_4IT_WP_VP
 {
     public partial class sendcode : Form
     {
-        string randomCode;
-        public static string to;
-        public static string from;
+       
         public sendcode()
         {
             InitializeComponent();
@@ -24,8 +22,25 @@ namespace SWP_4IT_WP_VP
 
         private void btn_SendCode_Click(object sender, EventArgs e)
         {
+            string from;
+            string to;
+            string randomCode;
+
+            Random rand = new Random();
+            randomCode = (rand.Next(999999)).ToString();
             
-            
+            from = "Inventurprogramm-Widauer-Payr@outlook.com";
+            to = txtbox_Email.Text;
+
+            SmtpClient client = new SmtpClient("smtp-mail.outlook.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("Inventurprogramm-Widauer-Payr@outlook.com", "INV12345?!"),
+                EnableSsl = true
+            };
+            client.Send(from, to, "Recovery Code", "Your recovery Code is: " + randomCode);
+
+            //recovery Code in Datenbank speichern
         }
     }
 }
