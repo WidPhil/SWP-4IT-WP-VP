a800a32b7528d2cb65cbbedb362d2fe214d83396
WidPhil <91456693+WidPhil@users.noreply.github.com>
2022-11-15 12:33:43 +0100

    Push (Ausversehen auf Main)

---
+++ SWP-4IT-WP-VP/sendcode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace SWP_4IT_WP_VP
+{
+    public partial class sendcode : Form
+    {
+        string randomCode;
+        public static string to;
+        public static string from;
+        public sendcode()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_SendCode_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+    }
+}
