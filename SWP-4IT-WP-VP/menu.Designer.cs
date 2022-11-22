fda784f1ec69f47cc26f45b1a2b42fcfc4b79ac2
Vanessa Payr <vanessa.payr@hak-kitz.ac.at>
2022-11-22 12:48:17 +0100

    Neuer Stand

--- SWP-4IT-WP-VP/menu.Designer.cs
+++ SWP-4IT-WP-VP/menu.Designer.cs
@@ -28,22 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelMainMenu = new System.Windows.Forms.Panel();
+            this.panelMenuLogo = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelMainMenu.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panelMainMenu
+            // 
+            this.panelMainMenu.BackColor = System.Drawing.Color.White;
+            this.panelMainMenu.Controls.Add(this.button1);
+            this.panelMainMenu.Controls.Add(this.panelMenuLogo);
+            this.panelMainMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMainMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMainMenu.Name = "panelMainMenu";
+            this.panelMainMenu.Size = new System.Drawing.Size(250, 513);
+            this.panelMainMenu.TabIndex = 0;
+            // 
+            // panelMenuLogo
+            // 
+            this.panelMenuLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelMenuLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelMenuLogo.Name = "panelMenuLogo";
+            this.panelMenuLogo.Size = new System.Drawing.Size(250, 100);
+            this.panelMenuLogo.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(392, 458);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1008, 513);
+            this.Controls.Add(this.panelMainMenu);
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "menu";
-            this.Text = "Main";
+            this.Text = "Intersport Home";
             this.Load += new System.EventHandler(this.menu_Load);
+            this.panelMainMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelMainMenu;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelMenuLogo;
     }
 }
